da3a01183aa3d19faec62a178652f42704e7e459
sumerzhang <zhangshaohua2015@gmail.com>
2018-04-04 09:34:14 +0800

    add requirements.txt and fabfile.py

---
+++ fabfile.py
@@ -0,0 +1,31 @@
+from fabric.api import local
+from fabric.decorators import task
+
+def hello():
+	"""
+	Test Fabric framework
+	"""
+	print("hello world")
+
+
+@task
+def install():
+	"""
+	Install requirements packages
+	"""
+	local("pip install -r requirements.txt")
+
+#def install(requirements_env="dev"):
+#	local("pip install -r requirments/%s.txt" % requirements_env)
+@task
+def runserver():
+	"""
+	Run Django server
+	"""
+	local("python manage.py runserver")
+
+@task
+def pep8():
+	"""Check the project for PEP8 compliance using `pep8`"""
+	local("pep8 .")
+
